0cf1ddcfff27b7238b947f0f97f83c058852398f
Peter Casinelli <pcas00@gmail.com>
2012-10-24 21:36:47 -0400

    Moving model into maps view controller and using a datasource that voices view controller can communicate with to manually add latitudes and longitudes

--- Voices/MapViewController.m
+++ Voices/MapViewController.m
@@ -7,21 +7,84 @@
 //
 
 #import "MapViewController.h"
+#import <MapKit/MapKit.h>
+#import "VoicesViewController.h"
+#import "Location.h"
 
-@interface MapViewController ()
-
+@interface MapViewController () <VoicesDataSource>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
 @implementation MapViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+@synthesize locationsDatabase = _locationsDatabase;
+@synthesize annotations = _annotations;
+
+
+-(void) updateMapView
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+    if (self.mapView.annotations) [self.mapView removeAnnotations:self.mapView.annotations];
+    if (self.annotations) [self.mapView addAnnotations:self.annotations];
 }
+
+- (void) setMapView:(MKMapView *)mapView
+{
+    _mapView = mapView;
+    [self updateMapView];
+}
+
+-(void) setAnnotations:(NSArray *)annotations
+{
+    _annotations = annotations;
+    [self updateMapView];
+}
+
+
+ - (IBAction)addLocationPressed:(id)sender {
+ 
+ double latitudeDouble = [self.latitude.text doubleValue];
+ double longitudeDouble = [self.longitude.text doubleValue];
+ 
+ NSDictionary *locationInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+ [NSNumber numberWithDouble:latitudeDouble],  @"latitude",
+ [NSNumber numberWithDouble:longitudeDouble], @"longitude",
+ self.locationTitle.text, @"title", nil];
+ 
+ [Location locationWithInfo:locationInfo
+ inManagedObjectContext:self.locationsDatabase.managedObjectContext];
+ 
+ NSLog(@"Add location was pressed and latitude is %@ and longitude is %@", self.latitude.text, self.longitude.text);
+ 
+ }
+
+
+//Check different cases of document state
+- (void) useDocument {
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[self.locationsDatabase.fileURL path]]) {
+        // does not exist on disk, so create it
+        [self.locationsDatabase saveToURL:self.locationsDatabase.fileURL forSaveOperation:UIDocumentSaveForCreating completionHandler:^(BOOL success) {
+            //[self setupFetchedResultsController];
+        }];
+    } else if (self.locationsDatabase.documentState == UIDocumentStateClosed) {
+        // exists on disk, but we need to open it
+        [self.locationsDatabase openWithCompletionHandler:^(BOOL success) {
+            //[self setupFetchedResultsController];
+        }];
+    } else if (self.locationsDatabase.documentState == UIDocumentStateNormal) {
+        // already open and ready to use
+        //[self setupFetchedResultsController];
+    }
+}
+
+- (void) setLocationsDatabase:(UIManagedDocument *)locationsDatabase
+{
+    if (_locationsDatabase != locationsDatabase){
+        _locationsDatabase = locationsDatabase;
+        [self useDocument];
+    }
+}
+
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -34,10 +97,31 @@
     }
 }
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    //Create database if necessary; then set up fetch results controller
+    if (!self.locationsDatabase){
+        NSURL *url = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+        url = [url URLByAppendingPathComponent:@"Default Locations Database"];
+        self.locationsDatabase = [[UIManagedDocument alloc] initWithFileURL:url];
+    }
 }
 
 - (void)didReceiveMemoryWarning
--- Voices/VoicesViewController.h
+++ Voices/VoicesViewController.h
@@ -8,12 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+@class VoicesViewController;
+
+@protocol VoicesDataSource
+- (IBAction)addLocationPressedWithLatitude: (double) latitude andLongitude: (double) longitude;
+@end
+
 @interface VoicesViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UITextField *latitude;
 @property (weak, nonatomic) IBOutlet UITextField *longitude;
 @property (weak, nonatomic) IBOutlet UITextField *locationTitle;
 
-@property (nonatomic, strong) UIManagedDocument *locationsDatabase;
+@property (nonatomic, weak) IBOutlet id <VoicesDataSource> dataSource;
 
 @end
