19090273e20357aeec5dba46f760a340a891df20
Peter Casinelli <pcas00@gmail.com>
2012-11-19 16:51:19 -0500

    Changed view controller to act as place to enter title of voice. This is segued to when a user presses Save on map view controller

--- Voices/VoicesViewController.h
+++ Voices/VoicesViewController.h
@@ -7,18 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Location.h"
+#import <MapKit/MapKit.h>
 
 @class VoicesViewController;
 
 @protocol VoicesDataSource
-- (IBAction)addLocationPressedWithTitle: (NSString *)title AndLatitude: (double) latitude andLongitude: (double) longitude;
+/*- (IBAction)addLocationPressedWithTitle: (NSString *)title AndLatitude: (double) latitude andLongitude: (double) longitude;*/
+-(Location *)addVoiceWithTitle:(NSString *)title;
+-(CLLocationCoordinate2D) getCurrentLocationCoordinates;
 @end
 
 @interface VoicesViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UITextField *latitude;
+/*@property (weak, nonatomic) IBOutlet UITextField *latitude;
 @property (weak, nonatomic) IBOutlet UITextField *longitude;
-@property (weak, nonatomic) IBOutlet UITextField *locationTitle;
+@property (weak, nonatomic) IBOutlet UITextField *locationTitle;*/
+@property (weak, nonatomic) IBOutlet UITextField *voiceTitle;
+@property (weak, nonatomic) IBOutlet UILabel *latitudeTextField;
+@property (weak, nonatomic) IBOutlet UILabel *longitudeTextField;
 
 @property (nonatomic, weak) IBOutlet id <VoicesDataSource> dataSource;
 
--- Voices/VoicesViewController.m
+++ Voices/VoicesViewController.m
@@ -9,6 +9,7 @@
 #import "VoicesViewController.h"
 #import "Location.h"
 #import "Location+Helper.h"
+#import <MapKit/MapKit.h>
 
 @interface VoicesViewController ()
 
@@ -16,9 +17,12 @@
 
 @implementation VoicesViewController
 
-@synthesize longitude = _longitude;
+/*@synthesize longitude = _longitude;
 @synthesize latitude = _latitude;
-@synthesize locationTitle = _locationTitle;
+@synthesize locationTitle = _locationTitle;*/
+@synthesize voiceTitle = _voiceTitle;
+@synthesize latitudeTextField = _latitudeTextField;
+@synthesize longitudeTextField = _longitudeTextField;
 @synthesize dataSource = _dataSource;
 
 //Hide the keyboard when user touches on background
@@ -28,10 +32,47 @@
     [super touchesBegan:touches withEvent:event];
 }
 
-- (IBAction)addLocation:(id)sender {
-    NSLog(@"Pressed add location");
+- (IBAction)addVoiceFromDataSource:(id)sender {
     
-    [self.dataSource addLocationPressedWithTitle:self.locationTitle.text AndLatitude:[self.latitude.text doubleValue] andLongitude:[self.longitude.text doubleValue]];
+    NSString *title = [[NSString alloc] init];
+    if (self.voiceTitle.text.length == 0)
+    {
+        NSDate *date = [NSDate date];
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc]init];
+        [dateFormat setDateFormat:@"HH:mm:ss zzz"];
+        NSString *dateString = [dateFormat stringFromDate:date];
+        
+        title = [NSString stringWithFormat:@"Voice at %@", dateString];
+    } else {
+        title = self.voiceTitle.text;
+    }
+    
+    Location *newVoice = [self.dataSource addVoiceWithTitle:title];
+    
+    if (newVoice == nil)
+    {
+        NSLog(@"Error adding new voice");
+    } else {
+        NSLog(@"Added successfully");
+        [self.navigationController popToRootViewControllerAnimated:YES];
+    }
 }
+- (IBAction)cancelButtonPressed:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    CLLocationCoordinate2D currentLocation = [self.dataSource getCurrentLocationCoordinates];
+
+    self.latitudeTextField.text = [NSString stringWithFormat:@"%g", currentLocation.latitude];
+    self.longitudeTextField.text = [NSString stringWithFormat:@"%g", currentLocation.longitude];
+    
+    [self.voiceTitle becomeFirstResponder];
+    NSLog(@"viewDidLoad VVC");
+}
+
 
 @end
